bf325a43d69e31ec470929fde04f38ade045ce73
Alex Klim <klimenko.tetiana3@gmail.com>
2023-05-09 18:56:22 +0200

    login form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, flash, request, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, logout_user
 
 app = Flask(__name__)
 
@@ -8,6 +10,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'rbgvfedcsxdcfvbfgnhb'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+
+        return f'User: {(self.login)} - {(self.password)}'
+
+user = User(login='Artist', password='1122')
 
 @app.route('/')
 def index():
@@ -32,23 +44,42 @@ def order():
     return render_template("order.html")
 
 
-@app.route('/login/', methods=['post', 'get'])
-def login():
-    message = ''
-    username = ''
-    password = 0
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        username = request.form.get('username')  # запрос к данным формы
+        login = request.form.get('login')
         password = request.form.get('password')
-        print(username, password)
+        user = User(login=login, password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            print(url_for('register'))
+            flash(f'Login or password is not correct - {login} - {password}: {user}')
+            return redirect('/')
+        except:
+            print("Something wrong")
+            return render_template('register.html')
 
-    if username == 'Artist' and password == '1111':
-        message = "Correct username and password"
     else:
-        message = "Wrong username or password"
+        return render_template('register.html')
 
-    return render_template('login.html', message=message)
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+        flash(f'Login or password is not correct - {user}')
+        if user and check_password_hash(user.password, password):
+            login_user(user)
 
+            next_page = request.args.get('next')
+            return redirect(next_page)
+        else:
+            flash(f'Login or password is not correct - {login} - {password}: {user}')
+    else:
+        flash('Please enter login and password')
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
